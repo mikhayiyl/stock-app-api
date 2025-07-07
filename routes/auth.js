05a02e10d9b49e42337632a3f8d538f531870351
mikhayiyl <duhnycan@gmail.com>
2025-07-07 16:06:05 +0300

    modify validate auth function using outdated joi version

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,28 @@
 const { User } = require("../models/user");
-const Joi = require('joi');
+const Joi = require("joi");
 const validator = require("../middleware/validator");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
+router.post("/", validator(validateAuth), async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid credentials");
 
-router.post('/', validator(validateAuth), async (req, res) => {
+  const password = await bcrypt.compare(req.body.password, user.password);
+  if (!password) return res.status(400).send("Invalid credentials");
 
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send("Invalid credentials");
+  const token = user.generateAuthToken();
 
-    const password = await bcrypt.compare(req.body.password, user.password);
-    if (!password) return res.status(400).send("Invalid credentials");
-
-    const token = user.generateAuthToken();
-
-    res.send(token);
+  res.send(token);
 });
 
-
-
 function validateAuth(auth) {
-    const schema = {
-        email: Joi.string().max(50).email().required(),
-        password: Joi.string().min(6).max(50).required(),
-    };
-    return Joi.validate(auth, schema);
-};
+  const schema = Joi.object({
+    email: Joi.string().email().max(254).required(),
+    password: Joi.string().min(6).max(72).required(),
+  });
 
-
+  return schema.validate(auth);
+}
 
 module.exports = router;
